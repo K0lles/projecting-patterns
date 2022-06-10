554fdb2c0f1e5deb02fd39ae9e0e81d9cb83cd42
oleksii kolotylo <oleksijkolotilo63@gmail.com>
2022-06-10 23:23:14 +0300

    singleton without previous excessive code

--- singleton.py
+++ singleton.py
@@ -3,7 +3,7 @@ class SingletonBase(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonBase, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
